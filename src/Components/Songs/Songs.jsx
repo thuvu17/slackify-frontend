c8c9f5e59c0c8a48a915553e7ca4699b7578f5b9
Thu Vu <tnv2002@nyu.edu>
2024-02-11 00:07:55 -0500

    added working game display

--- src/Components/Songs/Songs.jsx
+++ src/Components/Songs/Songs.jsx
@@ -4,146 +4,70 @@ import axios from 'axios';
 
 import { BACKEND_URL } from '../../constants';
 
-const SONGS_ENDPOINT = `${BACKEND_URL}/songs`;
+const SONGS_EP = `${BACKEND_URL}/songs`;
 
-function AddSongForm({
-  visible,
-  cancel,
-  fetchSongs,
-  setError,
-}) {
-  const [name, setName] = useState('');
-  const [artist, setArtist] = useState('');
-  const [album, setAlbum] = useState('');
-  const [genre, setGenre] = useState('');
-  const [bpm, setBpm] = useState(0);
-
-  const changeName = (event) => { setName(event.target.value); };
-  const changeArtist = (event) => { setArtist(event.target.value); };
-  const changeAlbum = (event) => { setAlbum(event.target.value); };
-  const changeGenre = (event) => { setGenre(event.target.value); };
-  const changeBpm = (event) => { setBpm(event.target.value); };
-
-
-  const addSong = (event) => {
-    event.preventDefault();
-    axios.post(SONGS_ENDPOINT, { name, artist, album, genre, bpm })
-      .then(fetchSongs)
-      .catch(() => { setError('There was a problem adding the song.'); });
-  };
-
-  if (!visible) return null;
-  return (
-    <form>
-      <label htmlFor="name">
-        Name
-      </label>
-      <input required type="text" id="name" value={name} onChange={changeName} />
-      <label htmlFor="artist">
-        Artist
-      </label>
-      <input required type="text" id="artist" onChange={changeArtist} />
-      <label htmlFor="album">
-        Album
-      </label>
-      <input required type="text" id="album" onChange={changeAlbum} />
-      <label htmlFor="genre">
-        Genre
-      </label>
-      <input required type="text" id="genre" onChange={changeGenre} />
-      <label htmlFor="bpm">
-        BPM
-      </label>
-      <input required type="number" id="bpm" onChange={changeBpm} />
-      <button type="button" onClick={cancel}>Cancel</button>
-      <button type="submit" onClick={addSong}>Submit</button>
-    </form>
-  );
-}
-AddSongForm.propTypes = {
-  visible: propTypes.bool.isRequired,
-  cancel: propTypes.func.isRequired,
-  fetchSongs: propTypes.func.isRequired,
-  setError: propTypes.func.isRequired,
-};
 
 function ErrorMessage({ message }) {
-  return (
-    <div className="error-message">
-      {message}
-    </div>
-  );
-}
-ErrorMessage.propTypes = {
-  message: propTypes.string.isRequired,
-};
-
-function Song({ song }) {
-  const { name, artist, album, genre, bpm } = song;
-  return (
-    <div className="song-container">
-      <h2>{name}</h2>
-      <p>
-              Artist: {artist}
-              Album: {album}
-              Genre: {genre}
-              BPM: {bpm}
-      </p>
-    </div>
-  );
-}
-Song.propTypes = {
-  song: propTypes.shape({
-  name: propTypes.string.isRequired,
-  artist: propTypes.string.isRequired,
-  album: propTypes.string.isRequired,
-  genre: propTypes.string.isRequired,
-  bpm: propTypes.number.isRequired,
-  }).isRequired,
-};
-
-function songsObjectToArray({ Data }) {
-  const keys = Object.keys(Data);
-  const songs = keys.map((key) => Data[key]);
-  return songs;
-}
-
-function Songs() {
-  const [error, setError] = useState('');
-  const [songs, setSongs] = useState([]);
-  const [addingSong, setAddingSong] = useState(false);
-
-  const fetchSongs = () => {
-    axios.get(SONGS_ENDPOINT)
-      .then(({ data }) => setSongs(songsObjectToArray(data)))
-      .catch(() => setError('There was a problem retrieving the list of songs.'));
+    return (
+      <div className="error-message">
+        {message}
+      </div>
+    );
+  }
+  ErrorMessage.propTypes = {
+    message: propTypes.string.isRequired,
   };
 
-  const showAddSongForm = () => { setAddingSong(true); };
-  const hideAddSongForm = () => { setAddingSong(false); };
 
-  useEffect(fetchSongs, []);
+function Songs() {
+    const [songs, set_songs] = useState([]);
+    const [error, set_error] = useState('');
+    
+    useEffect(
+        () => {
+            axios.get(SONGS_EP)
+                .then((response) => {
+                    const songs_object = response.data.Data;
+                    const keys = Object.keys(songs_object);
+                    const songs_array = keys.map((key) => songs_object[key]);
+                    set_songs(songs_array);
+                })
+                .then()
+                .catch((error) => { 
+                    if (error.response) {
+                        // The request was made and the server responded with a status code
+                        console.error('Server Error:', error.response.data);
+                        set_error('Server Error: ' + error.response.data.message); // Assuming the server sends error messages in a 'message' field
+                    } else if (error.request) {
+                        // The request was made but no response was received
+                        console.error('Network Error:', error.request);
+                        set_error('Network Error: No response from server');
+                    } else {
+                        // Something else happened while setting up the request
+                        console.error('Request Error:', error.message);
+                        set_error('Request Error: ' + error.message);
+                    }
+                });
+                
+        },
+        [],
+    );
 
-  return (
+    return (
     <div className="wrapper">
-      <header>
-        <h1>
-          View All Songs
-        </h1>
-        <button type="button" onClick={showAddSongForm}>
-          Add a Song
-        </button>
-      </header>
-      <AddSongForm
-        visible={addingSong}
-        cancel={hideAddSongForm}
-        fetchSongs={fetchSongs}
-        setError={setError}
-      />
-      {error && <ErrorMessage message={error} />}
-      {songs.map((song) => <Song key={song.name} song={song} />)}
+        <h1>View All Songs</h1>
+        {error && <ErrorMessage message={error} />}
+        {songs.map((song) => (
+            <div className='song-container' key={song._id}>
+                <h2>{song.name}</h2>
+                <p>Aritst: {song.artist}</p>
+                <p>Album: {song.album}</p>
+                <p>Genre: {song.genre}</p>
+                <p>BPM: {song.bpm}</p>
+            </div>
+        ))}
     </div>
-  );
+    );
 }
 
 export default Songs;
