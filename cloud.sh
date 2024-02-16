2d7ebfa58865fb81288b2a57181bf5cd205e1a53
Quan Phung <quan.phung@ecotruck.vn>
2024-02-16 15:38:22 -0500

    Added cloud.sh script to run frontend on cloud server

--- cloud.sh
+++ cloud.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# Run your React frontend against a local API server.
+# Run your React frontend against a cloud API server.
 export REACT_APP_URL_PRE="http://slackify.pythonanywhere.com/"
 npm start
