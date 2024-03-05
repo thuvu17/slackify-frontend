918e78adaf4141d9e68c07483223a9e2ce77ef39
Yongwen Lei <yl7690@nyu.edu>
2024-03-05 11:29:23 -0500

    changed pythonanywhere url to https

--- cloud.sh
+++ cloud.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Run your React frontend against a cloud API server.
-export REACT_APP_URL_PRE="http://slackify.pythonanywhere.com/"
+export REACT_APP_URL_PRE="https://slackify.pythonanywhere.com/"
 npm start
