4c7c25df6aee8bb2d89856ac08187074245cd4c1
Thu Vu <tnv2002@nyu.edu>
2024-03-11 18:48:20 -0400

    edited homepage and add photo

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,31 +1,16 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-// import { Helmet } from "react-helmet";
-import Row from "react-bootstrap/Row";
-import Card from "react-bootstrap/Card";
-// import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 
 function Home() {
     return (
        <>
-       <Container>
-         <Row>
-            <Card className="home-card" style={{ width: '18rem' }}>
-               <Card.Img variant="top" src="./public/logo192.png"/>
-               <Card.Body>
-               <Card.Title className="card-title">View Our Songs</Card.Title>
-                  <Card.Text> 
-                     From heart-pounding beats to adrenaline-pumping melodies, elevate your workout with the perfect soundtrack. 
-                     Make every rep count with Slackify!
-                  </Card.Text>
-                  <a href="/songs">
-                     <Button className="home-button">Go to Songs</Button>
-                  </a>
-               </Card.Body>
-               </Card>
-            </Row>
-       </Container>
+          <div className="home_title">View Our Songs</div>
+          <div className="home_body" style={{
+             textAlign: 'center', color: 'white',
+             fontSize: 20,
+          }}>
+            <p>From heart-pounding beats to adrenaline-pumping melodies, elevate your workout with the perfect soundtrack. </p>
+             <p>Make every rep count with Slackify!</p></div>
+          <img className="photo" src={require('.//assets/workout.jpeg')}></img>
        </>
 
     );
