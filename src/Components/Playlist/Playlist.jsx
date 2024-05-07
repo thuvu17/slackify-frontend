bb05bf6ca09978549cc34ca9e2c12bd7aedd3960
Yongwen Lei <yl7690@nyu.edu>
2024-05-07 02:24:53 -0400

    added update playlist name feature

--- src/Components/Playlist/Playlist.jsx
+++ src/Components/Playlist/Playlist.jsx
@@ -5,11 +5,14 @@ import propTypes from 'prop-types';
 import axios from 'axios';
 
 import { BACKEND_URL } from '../../constants';
+import { useAuth } from '../AuthProvider/AuthProvider';
 
 const PLAYLIST_EP = `${BACKEND_URL}/playlist`;
 const PLAYLISTS_EP = `${BACKEND_URL}/playlists`;
+const UPDATE_PLAYLIST_NAME_EP = `${PLAYLISTS_EP}/Update`;
 const DEL_PLAYLIST_EP = `${PLAYLISTS_EP}/delete`;
 const PLAYLISTS_URL = '/playlists';
+const PLAYLIST_URL = '/playlist';
 
 function SongsObjectToArray(Data) {
   const keys = Object.keys(Data);
@@ -31,6 +34,50 @@ ErrorMessage.propTypes = {
 };
 
 
+function UpdatePlaylistNameForm({
+  name,
+  visible,
+  cancel,
+  setError,
+}) {
+  const [newName, setNewName] = useState('');
+  const { user_id } = useAuth();
+  const changeNewName = (event) => { setNewName(event.target.value); };
+  const navigate = useNavigate();
+
+  const UpdatePlaylistName = (event) => {
+    event.preventDefault();
+    axios.put(`${UPDATE_PLAYLIST_NAME_EP}/${user_id}/${name}/${newName}`)
+      .then(() => {
+        navigate(`${PLAYLIST_URL}/${user_id}/${newName}`, {replace: true});
+        window.location.reload();
+      }
+      )
+      .catch(error => {
+        if (error.response) {
+          // console.error(error.response.data);
+          setError(error.response.data.message);
+        }
+      });
+  };
+
+  if (!visible) return null;
+  return (
+    <form className='changeNameForm'>
+      <input required type="text" id="name" value={newName} onChange={changeNewName} className='name-input'/>
+      <button type="submit" onClick={UpdatePlaylistName}>Submit</button>
+      <button type="button" onClick={cancel}>Cancel</button>
+    </form>
+  );
+}
+
+UpdatePlaylistNameForm.propTypes = {
+  visible: propTypes.bool.isRequired,
+  cancel: propTypes.func.isRequired,
+  setError: propTypes.func.isRequired,
+};
+
+
 function Playlist() {
   const [songs, setSongs] = useState([]);
   const [thisName, setName] = useState('');
@@ -38,6 +85,12 @@ function Playlist() {
   const [error, setError] = useState('');
   const { user_id, name } = useParams();
   const navigate = useNavigate();
+  const [changingName, setChangingName] = useState(false);
+  const showChangeNameForm = () => { setChangingName(true); };
+  const hideChangeNameForm = () => { 
+    setChangingName(false); 
+    setError('');
+  };
    
   useEffect(
     () => {
@@ -72,7 +125,6 @@ function Playlist() {
     navigate(`${PLAYLISTS_URL}`);
   };
 
-
   const delPlaylist = (user_id, name) => {
     axios.delete(`${DEL_PLAYLIST_EP}/${user_id}/${name}`)
     .then(() => {
@@ -87,15 +139,25 @@ function Playlist() {
   return (
   <div className="wrapper">
     <div><button className='return-button' onClick={handleReturn}>	&lt; Return</button></div>
-    <h1>{thisName}</h1>
     <div className='playlist-header'>
+    { !changingName && <h1>{thisName}</h1> }
+    { !changingName && <button type="button" onClick={showChangeNameForm} className='playlist-name-button'></button> }
+    <UpdatePlaylistNameForm
+      name={name}
+      visible={changingName}
+      cancel={hideChangeNameForm}
+      setError={setError}
+    />
+    </div>
+    
+    <div className='playlist-subheader'>
       <h3>Date Created: {thisDate}</h3>
       <button className="del-button" onClick={() => delPlaylist(user_id, thisName)}>Delete</button>
     </div>
 
-    
+    {error && <ErrorMessage message={error} />}
     {songs.length === 0 ? (
-      <p>No songs found in this playlist.</p>
+      <p>There is no song in the playlist. Go add one now.</p>
     ):(
       songs.map((song) => (
         <div className='playlist-song-container' key={song._id}>
@@ -107,7 +169,7 @@ function Playlist() {
       </div>
     ))
   )}
-    {error && <ErrorMessage message={error} />}
+    
     
   </div>
   );
